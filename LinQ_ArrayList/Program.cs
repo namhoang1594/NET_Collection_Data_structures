4f8ebc618d59578aef11f3581a4c4fd11b9635dd
namhoang1594 <hnn.nam01@gmail.com>
2020-09-29 20:10:34 +0700

    Add project files.

---
+++ LinQ_ArrayList/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+namespace LinQ_ArrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList array = new ArrayList();
+            array.Add(new Staff(1, "Nam", 24));
+            array.Add(new Staff(2, "Kien", 21));
+            array.Add(new Staff(3, "Viet", 21));
+            array.Add(new Staff(4, "Hang", 23));
+            array.Add(new Staff(5, "Tien", 24));
+
+            var newArrayList = array.OfType<Staff>();
+            var Search = from name in newArrayList
+                         where (name.Age < 24)
+                         select name;
+            var sapxep = from name in newArrayList
+                         orderby name.Name
+                         select name;
+            Console.WriteLine("Nhung nguoi nho hon 24 tuoi: ");
+            foreach (var item in Search)
+            {
+                Console.WriteLine("ID: " + item.ID + ", Name: " + item.Name + ", Age: " + item.Age);
+            }
+            Console.WriteLine("Sap xep theo ten: ");
+            foreach (var item in sapxep)
+            {
+                Console.WriteLine("ID: " + item.ID + ", Name: " + item.Name + ", Age: " + item.Age);
+            }
+        }
+    }
+}
---
+++ LinQ_ArrayList/Staff.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinQ_ArrayList
+{
+    class Staff
+    {
+        public int ID { set; get; }
+        public string Name { set; get; }
+        public int Age { set; get; }
+        public Staff(int ID, string Name, int Age)
+        {
+            this.ID = ID;
+            this.Name = Name;
+            this.Age = Age;
+        }
+    }
+}
